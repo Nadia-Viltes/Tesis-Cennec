981724b282634aad7f5e7cb2ca8e7d1f3aeb6cbb
unknown <diego4d@gmail.com>
2021-07-07 00:56:48 -0300

    Modificaciones base de datos

--- bd_config.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-
-def establecer_conexion():
-    return pymysql.connect(host='localhost',
-                                user='root',
-                                password='admin',
-                                db='mydb')
