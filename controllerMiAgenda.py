0e0a75d593817d66980e811e7e04e957981af1e4
Nadia-Viltes <85589825+Nadia-Viltes@users.noreply.github.com>
2021-11-29 01:43:21 -0300

    Hay muchos cambios: home,eliminar  turnos admisión, manejo manesaje cuando no hay turnos adm cuando asignas turno

--- controllerMiAgenda.py
+++ controllerMiAgenda.py
@@ -1,21 +1,23 @@
+from loguru import logger
 from config_bd import get_conexion
 
 # query para que me muestre los datos en la lista de turnos
-def obtener_lista_turno_mi_agenda(usuario, usuario_rol):
+def obtener_lista_turno_mi_agenda(usuario,usuario2):
     query = """
             SELECT tur.IdTurno, est.Nombre, DATE_FORMAT(tur.FechaTurno, '%d/%m/%Y'), DATE_FORMAT(tur.HoraDesde, '%H:%i'), 
-            DATE_FORMAT(tur.HoraHasta, '%H:%i'), pac.IdPaciente,pac.Nombre, pac.Apellido, pac.NumeroDocumento, hcd.IdHistoriaClinica	
+            DATE_FORMAT(tur.HoraHasta, '%H:%i'), pac.IdPaciente,pac.Nombre, pac.Apellido, pac.NumeroDocumento, hcd.IdHistoriaClinica, u.Nombre	
             FROM turno as tur, paciente as pac, estadoturno as est, historiaclinica as hcd, usuario as u, recurso as rec, 
-            profesional as pro, rol as rol
-            WHERE tur.IdPaciente = pac.IdPaciente
-            AND pac.IdPaciente = hcd.IdPaciente
-            AND est.IdEstadoTurno = tur.IdEstadoTurno
-            AND u.IdRecurso = rec.IdRecurso
-            AND rec.IdRecurso = pro.IdRecurso
-            AND u.IdRol = rol.IdRol
+            profesional as pro, especialidad as esp
+            WHERE hcd.IdPaciente = pac.IdPaciente
+            AND pac.IdPaciente = tur.IdPaciente
+            AND tur.IdEstadoTurno = est.IdEstadoTurno
+            AND tur.IdEspecialidad = esp.IdEspecialidad
+            AND esp.IdEspecialidad = pro.IdEspecialidad
+            AND pro.IdRecurso = rec.IdRecurso
+            AND rec.IdRecurso = u.IdRecurso
             AND tur.FechaBaja is null
-            AND (u.IdUsuario = {} OR lower('{}') = 'administrador');
-            """.format(usuario, usuario_rol)
+            AND (tur.IdProfesionalAsignado = {} OR tur.IdProfesionalReceptado = {});
+            """.format(usuario,usuario2)
     conexion = get_conexion()
     turnoProfesional = []
     with conexion.cursor() as cur:
@@ -25,6 +27,38 @@ def obtener_lista_turno_mi_agenda(usuario, usuario_rol):
     return turnoProfesional
 
 
+# Query para obtener datos del usuario logueado y rellenar datos en la hcd
+def obtener_datos_usuario_profesional(usuario):
+    query = """
+            SELECT us.IdUsuario, re.Nombre, re.Apellido, esp.Nombre, prof.IdProfesional
+            FROM usuario AS us, recurso AS re, profesional as prof, especialidad as esp
+            WHERE us.IdRecurso = re.IdRecurso
+            AND re.IdRecurso = prof.IdRecurso
+            AND prof.IdEspecialidad = esp.IdEspecialidad
+            AND us.IdUsuario = {};""".format(usuario)
+    conexion = get_conexion()
+    usuarioProfesional = []
+    with conexion.cursor() as cur:
+        cur.execute(query)
+    usuarioProfesional = cur.fetchone()
+    conexion.close()
+    return usuarioProfesional
+
+# query para obtener los datos del turno
+def obtener_turno_atendiendo(idTurno):
+    query = """
+           SELECT IdTurno, IdTipoTurno, IdEspecialidad, IdPaciente, FechaTurno, HoraDesde, HoraHasta, IdEstadoTurno, 
+           FechaInicioAtencion, IdUsuarioInicioAtencion, IdTurnoOriginal
+            FROM turno
+            WHERE IdTurno = {}""".format(idTurno)
+    conexion = get_conexion()
+    idTurnoAtendiendo = []
+    with conexion.cursor() as cur:
+        cur.execute(query)
+    idTurnoAtendiendo = cur.fetchone()
+    conexion.close()
+    return idTurnoAtendiendo
+    
 ## Agregar un nuevo turno en estado ATENDIENDO:
 def insertar_turno_atendiendo(tipoTurno, idEspecialidadDropdown, idPacienteAsignarTurno, fechaTurno, horaDesde, horaHasta, idEstadoTurno, idProfesionalDropdown, IdTurno):
     conexion = get_conexion()
@@ -94,19 +128,24 @@ def consulta_existe_evolucion(id):
 		   WHERE IdHistoriaClinica = {};""".format(id)
     print("Este es mi ver si existe evolucion -> {}".format(query))   
     conexion = get_conexion()
-    with conexion.cursor() as cur:
-        cur.execute(query)
-    existe_evolucion = cur.fetchone()[0]
-    conexion.close()
+    existe_evolucion = None
+    try:
+        with conexion.cursor() as cur:
+            cur.execute(query)
+        existe_evolucion = cur.fetchone()[0]
+    except:
+        logger.info("Ocurrio un error obteniendo datos")
+    finally:        
+        conexion.close()
     return existe_evolucion
 
 
 ## INSERTAR EVOLUCION
-def insertar_evolucion (idHCD):
+def insertar_evolucion (idHCD,usuario):
     conexion = get_conexion()
     query = """
-        INSERT INTO evolucion (IdHistoriaClinica, FechaAlta)
-        VALUES ({}, now())""".format(idHCD)
+        INSERT INTO evolucion (IdHistoriaClinica, FechaAlta, IdUsuarioAlta)
+        VALUES ({}, now(),{})""".format(idHCD,usuario)
     idEvolucion = None
     print("Insertar idEvolucion -> {}".format(query))
     with conexion.cursor() as cur:
@@ -119,11 +158,11 @@ def insertar_evolucion (idHCD):
 
 
 ## INSERTAR DETALLE DE ESA EVOLUCION
-def insertar_detalle (idEvolucion,idProfesional,observacion):
+def insertar_detalle (idEvolucion, idTurno, idProfesional, observacion, usuario):
     conexion = get_conexion()
     query = """
-        INSERT INTO detalleevolucion (IdEvolucion, IdTurno, IdProfesional, ObservacionAvance, FechaAlta)
-		VALUES({},{},{},'{}',now())""".format(idEvolucion,idProfesional,observacion)
+        INSERT INTO detalleevolucion (IdEvolucion, IdTurno, IdProfesional, ObservacionAvance, FechaAlta, IdUsuarioAlta)
+		VALUES({},{},{},'{}',now(),{})""".format(idEvolucion, idTurno, idProfesional, observacion, usuario)    
     idEvolucion = None
     print("Insertar detalle -> {}".format(query))
     with conexion.cursor() as cur:
@@ -132,3 +171,14 @@ def insertar_detalle (idEvolucion,idProfesional,observacion):
     conexion.commit()
     conexion.close()
     return idEvolucion
+
+
+## Operación para modificar el detall de la HCD
+def actualizar_detalle(inputIdEvolucion,inputIdTurnoHis,inputProfesionalHis,InputTextareaEvolucionesHis,usuario,inputIdDetEvo):
+    conexion = get_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute("""
+        UPDATE detalleevolucion SET IdEvolucion={}, IdTurno={}, IdProfesional={}, ObservacionAvance='{}',FechaModificacion=NOW(), IdUsuarioModificacion={}
+        WHERE IdDetalleEvolucion = {} """.format(inputIdEvolucion,inputIdTurnoHis,inputProfesionalHis,InputTextareaEvolucionesHis,usuario,inputIdDetEvo))
+    conexion.commit()
+    conexion.close()
