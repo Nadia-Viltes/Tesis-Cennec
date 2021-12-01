5412ee985095c04da3e8232236172170dc2ee506
Nadia-Viltes <85589825+Nadia-Viltes@users.noreply.github.com>
2021-11-30 21:08:07 -0300

    Para renegar

--- app.py
+++ app.py
@@ -290,10 +290,14 @@ def obtener_evolucion_id(id):
 
 @app.route('/hcd/modal/ver_historial/<int:id>', methods=["GET", "POST"])
 def ver_historial_hcd(id):
-    values={
-        'titulo': 'Este es un historial'
-    }
-    return jsonify({'htmlresponse': render_template('hcd_ver_historial.html',data=values)})
+    logger.info("llego a ver historial hcd {}".format(id))
+    #historial_hcd = obtener_historial_evoluciones(id)
+    #values={
+    #    'titulo': 'Este es un historial',
+    #    'historial': historial_hcd
+    #}
+    #return jsonify({'htmlresponse': render_template('hcd_ver_historial.html', data=values)})
+    return jsonify({'htmlresponse': render_template('hcd_ver_historial.html')})
 
 #Carga los turnos admision llenando la tabla con jquery
 @app.route('/hcd/agrega_turnos_admision', methods=["POST"])
